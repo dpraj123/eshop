1165312d583f73d422f603ccf0714cd9476f1392
dpraj123 <rajdp764@gmail.com>
2025-07-27 21:32:40 +0530

    package updat;

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,14 +1,44 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+const host = process.env.HOST ?? "localhost";
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API api-gateway'});
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many requests, please try again later!" },
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: any) => req.ip,
+});
+app.use(limiter);
+
+app.use("/", proxy("http://localhost:6001"));
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API api-gateway" });
 });
 
 app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] http://${host}:${port}`);
 });
